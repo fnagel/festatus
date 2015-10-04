309761dfd109d4e884fb20f6f3d3813a787c9f50
Felix Nagel <info@felixnagel.com>
2015-10-04 22:29:17 +0200

    Update requirements to TYPO3 7.5

--- ext_emconf.php
+++ ext_emconf.php
@@ -23,7 +23,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'author_email' => 'info@felixnagel.com',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '6.2.0-7.4.99',
+			'typo3' => '6.2.0-7.5.99',
 			'reports' => '',
 		),
 		'conflicts' => array(
