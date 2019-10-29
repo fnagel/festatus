d73f55359c2114bf9e7584b66b3e81df9ebc975d
Felix Nagel <info@felixnagel.com>
2019-10-29 23:43:09 +0100

    [TASK] Adopt license

--- Classes/Report/CheckFrontendStatus.php
+++ Classes/Report/CheckFrontendStatus.php
@@ -2,28 +2,12 @@
 
 namespace TYPO3\FrontendStatus\Report;
 
-/***************************************************************
-*  Copyright notice
-*
-*  (c) 2011-2016 Felix Nagel <info@felixnagel.com>
-*  All rights reserved
-*
-*  This script is part of the TYPO3 project. The TYPO3 project is
-*  free software; you can redistribute it and/or modify
-*  it under the terms of the GNU General Public License as published by
-*  the Free Software Foundation; either version 2 of the License, or
-*  (at your option) any later version.
-*
-*  The GNU General Public License can be found at
-*  http://www.gnu.org/copyleft/gpl.html.
-*
-*  This script is distributed in the hope that it will be useful,
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*  GNU General Public License for more details.
-*
-*  This copyright notice MUST APPEAR in all copies of the script!
-***************************************************************/
+/**
+ * This file is part of the "fe_status" Extension for TYPO3 CMS.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ */
 
 use TYPO3\CMS\Reports\StatusProviderInterface;
 use TYPO3\CMS\Reports\ExtendedStatusProviderInterface;
